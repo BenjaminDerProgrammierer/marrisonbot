3a58cf87ddb88fb93d692fef872c0ebbb7816998
BenjaminDerProgrammierer <benjamin.p.hartmann@gmail.com>
2022-07-08 16:55:11 +0200

    read messages

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 //import packages
 require('dotenv').config()
 const Discord = require('discord.js')
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 
 
 
 //The real bot code
-client.on("ready", () => {
-    console.log("Bot is ready")
-})
+client.once('ready', c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
 
-client.on("message", msg => {
-    if (msg.content === "hey") {
-        msg.reply("hi there")
-    } else if (msg.content === "u good bro") {
-        msg.channel.send("nah")
-    }
-})
+client.on('messageCreate', msg => {
+	console.log(`${JSON.stringify(msg.member.displayName)} sent a message in #${msg.channel.name}.`);
+});
 
 //connect this code to our discord_bot
 client.login(process.env.BOT_TOKEN)
