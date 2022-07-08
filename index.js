1dd0b919f9c200c451b3b57a1d30a378dd4388e8
BenjaminDerProgrammierer <benjamin.p.hartmann@gmail.com>
2022-07-08 20:12:58 +0200

    exported the EventListener functions to events

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-const Discord = require('discord.js')
+require('dotenv').config();
+const path = require('path');
+const fs = require('fs');
+const Discord = require('discord.js');
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
-const eventsPath = path.join(__dirname, 'events')
+const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.login(process.env.BOT_TOKEN);
-
-for (const file of eventFiles) {
+for (let file of eventFiles) {
     const filePath = path.join(eventsPath, file);
     const event = require(filePath);
     
@@ -16,3 +16,5 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
+client.login(process.env.BOT_TOKEN);
