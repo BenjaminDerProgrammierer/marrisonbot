1362b523fe37460b5b393b2e9857b78629f286c6
BenjaminDerProgrammierer <benjamin.p.hartmann@gmail.com>
2022-07-10 19:00:01 +0200

    Added Slash commands

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Disc
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
+//Chats u. Ä.
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	console.log(
+		`${interaction.member.displayName} sent a command in #${interaction.channel.name}: ${interaction.message}`
+	  );
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
 for (let file of eventFiles) {
     const filePath = path.join(eventsPath, file);
     const event = require(filePath);
