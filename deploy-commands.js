1755d8e03d077fc1d99336617fe728d6e2a3ad39
BenjaminDerProgrammierer <benjamin.p.hartmann@gmail.com>
2022-07-12 18:21:26 +0200

    exported commands to seperate files

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,16 +2,21 @@ require('dotenv').config();
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const fs = require('node:fs');
+const path = require('node:path');
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-].map(command => command.toJSON());
-
+const commands = []
 const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
 
 rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 require('dotenv').config();
-const path = require('path');
-const fs = require('fs');
+const fs = require('node:fs');
+const path = require('node:path');
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+//events
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-//Chats u. Ä.
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-	console.log(
-		`${interaction.member.displayName} sent a command in #${interaction.channel.name}: ${interaction.message}`
-	  );
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	}
-});
 for (let file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    
-    if (event.once) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	
+	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
 	} else {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+//commands
+client.commands = new Discord.Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+//Chats u. Ä.
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	console.log(
+		`${interaction.member.displayName} sent a command in #${interaction.channel.name}: ${interaction.commandName}`
+	);
+	const command = client.commands.get(interaction.commandName);
 
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+
+});
+	
 client.login(process.env.BOT_TOKEN);
